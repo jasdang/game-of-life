4f1ba535733c3f372c53adb2a6c32beb08e47f99
Jasmine Dang <ngandth.1992@gmail.com>
2020-06-14 13:34:08 +0800

    addEventListener to input form

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import _ from 'lodash';
-function component() {
-  const element = document.createElement('div');
+// import _ from 'lodash';
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// function component() {
+//   const element = document.createElement('div');
 
-  return element;
-}
+//   // Lodash, now imported by this script
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-document.body.appendChild(component());
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+console.log("Hello from src/index.js!");
+
+const btnCreateTable = document.getElementById('create-table');
+btnCreateTable.addEventListener('submit', (e) => {
+  event.preventDefault();
+  const row = e.currentTarget.querySelector('#row').value;
+  const column = e.currentTarget.querySelector('#column').value;
+  console.log(row);
+  console.log(column);
+});

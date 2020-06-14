578bfc1d02fb74e18819717cf4bcae7528364cbb
Jasmine Dang <ngandth.1992@gmail.com>
2020-06-14 15:12:51 +0800

    Created a new board based on user input

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-// import _ from 'lodash';
-
-// function component() {
-//   const element = document.createElement('div');
-
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
-
-console.log("Hello from src/index.js!");
-
 const btnCreateTable = document.getElementById('create-table');
+
 btnCreateTable.addEventListener('submit', (e) => {
   event.preventDefault();
   const row = e.currentTarget.querySelector('#row').value;
   const column = e.currentTarget.querySelector('#column').value;
-  console.log(row);
-  console.log(column);
+  document.getElementById('board').innerHTML = createNewBoard(row, column);
 });
+
+const createNewBoard = (r, c) => {
+  const board = document.createElement('table');
+  for (let i = 0; i < r; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < c; j++) {
+      const cell = document.createElement('td');
+      cell.innerText = '0';
+      row.append(cell);
+    }
+    board.append(row);
+  }
+  return board.innerHTML;
+}
